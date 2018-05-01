466746a554f615b39d4c0f1a65e57c470d18a345
Jake Quincey <jquincey@wisc.edu>
2018-05-01 14:22:19 -0500

    Working solution to file function.

--- solution_to_file.py
+++ solution_to_file.py
@@ -1,8 +1,7 @@
 import csv
-import argparse
-import tables as tb
 
-def solution_to_file(u, x, t)
+
+def solution_to_file(u, x, t):
     """
     Take the solution matrix of displacement (u), the time mesh (t), and the length mesh (x) and generate a .csv 
     file.
@@ -50,7 +49,19 @@ def solution_to_file(u, x, t)
             seq_idx += 1
 
     """
-    #Assuming that u is a numpy matrix
+    #Build header row of each value in t
+    #Write header row into file
+    #For each data row include the 
+    #    Attach x-value to displacement data
+    #    Write data row
+    time_length = len(t) #Number of time nodes and "x" entries in csv file
+    sol_header = [" "] + [t[idx] for idx in range(time_length)] #Building of header file
+    
+    with open("solution.csv","w") as csvfile:
+        sol_writer = csv.writer(csvfile, delimiter=",")
+        sol_writer.writerow(sol_header)
+        for idx in range(len(x)):
+            sol_writer.writerow([x[idx]] + [u[idx][col] for col in range(time_length)])
         
     
     

a3cbe773dd059d51f58d541bc79a468ea1098697
Eric Riewski <riewski@wisc.edu>
2018-05-01 14:23:15 -0500

    Added more changed to the main_Function _Simulations.py

--- main_Function_Simulation.py
+++ main_Function_Simulation.py
@@ -3,9 +3,12 @@ from def_Courant_Number import def_Courant_Number
 def main_Function_Simulation(user_input, u):
 
     c = def_Courant_Number(user_input['Time Step'],user_input['Space Step'],user_input['Wave Speed'])
+
+    u1 = u
+    u2 = u
     
-    for j in range(2,user_input['Temporal Nodes']):
-        for i in range(1,user_input['Spatial Nodes']):
+    for j in range(2,int(user_input['Temporal Nodes'])-1):
+        for i in range(1,int(user_input['Spatial Nodes'])-1):
             u1[j,i] = 2 * u[j-1,i] - u[j-2,i]
             u2[j,i] = u[j-1,i-1] - 2*u[j-1,i] + u[j-1,i+1]
             u[j,i] = u1[j,i] + c*c*u2[j,i]

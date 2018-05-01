35ebd544e2adeb5426e7aed54b70fd20a15598d0
Anthony Wolf <ajwolf4@wisc.edu>
2018-05-01 12:35:01 -0500

    changed keys in dictionary

--- get_user_input.py
+++ get_user_input.py
@@ -42,13 +42,6 @@ def check_plotting_times(sim_time, plot_times):
                 exit()
 
 
-def check_courant_number(courant_number):
-
-    if courant_number >= 1 or courant_number <= 0:
-        print("The courant number entered is not valid.\nProgram ended.")
-        exit()
-
-
 def main():
 
     parser = argparse.ArgumentParser()
@@ -62,28 +55,20 @@ def main():
     parser.add_argument("-pd", "--pluck_displacement", type=float, default=1,
                         required=False, help="Displacement of the pluck.")
 
-    parser.add_argument("-t", "--time", type=float, default=50, required=False,
+    parser.add_argument("-t", "--time", type=float, default=10, required=False,
                         help="Time of solution.")
 
     parser.add_argument("-pt", "--plot_times", nargs='+', type=float,
                         required=False,
                         help="Times at which to plot the solution.")
 
-    parser.add_argument("-dt", "--time_steps", type=float, default=100,
+    parser.add_argument("-dt", "--time_step", type=float, default=0.001,
                         required=False,
-                        help="The number of time nodes in the solution.")
+                        help="The time step of the solution.")
 
     parser.add_argument("-y", "--yield_stress", type=float, default=215,
                         required=False,
                         help="Yield strength of string material.")
-
-    parser.add_argument("-cn", "--courant_number", type=float, default=0.75,
-                        required=False, help="The Courant Number is " +
-                        "u*dt*dx and must be less than 1.")
-
-    parser.add_argument("-v0", "--initial_velocity", type=float, default=0,
-                        required=False, help="Initial velocity of the " +
-                        "string at the pluck position")
 
     parser.add_argument("-c", "--wave_speed", type=float, default=330,
                         required=False, help="Speed of the wave in meters" +
@@ -92,24 +77,31 @@ def main():
     parser.add_argument("-E", "--youngs_modulus", type=float, default=190000,
                         required=False, help="Young's modulus of material.")
 
+    parser.add_argument("-f", "--frequency", type=float, default=10,
+                        required=False, help="Source frequency.")
+
+    parser.add_argument("-x", "--space_step", type=float, default=1,
+                        required=False, help="Step size in space.")
+
     args = parser.parse_args()
 
     check_pluck_position(args.string_length, args.pluck_position)
     check_pluck_displacement(args.string_length, args.pluck_position,
                              args.pluck_displacement, args.yield_stress,
                              args.youngs_modulus)
-    check_courant_number(args.courant_number)
     check_plotting_times(args.time, args.plot_times)
+
+    temporal_nodes = args.time / args.time_step
 
     user_input = {'String Length': args.string_length,
                   'Pluck Position': args.pluck_position,
                   'Pluck Displacement': args.pluck_displacement,
                   'Time': args.time,
-                  'Time Steps': args.time_steps,
+                  'Time Step': args.time_step,
+                  'Temporal Nodes': temporal_nodes,
                   'Plotting Times': args.plot_times,
-                  'Courant Number': args.courant_number,
-                  'Initial Velocity': args.initial_velocity,
-                  'Wave Speed': args.wave_speed}
+                  'Wave Speed': args.wave_speed,
+                  'Frequency': args.frequency}
 
     return user_input
 

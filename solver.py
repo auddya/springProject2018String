14b9ebb3062d4917cf6bf518ac35170cca9dcd7c
Debabrata Auddya <auddya@wisc.edu>
2018-05-01 13:39:46 -0500

    Minor changes in the solutin matrix

--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ def solver(user_input):
 
     x = np.zeros(user_input['Spatial Nodes'])
     t = np.zeros(user_input['Temporal Nodes'])
-    u = np.zeros((x,t))
+    u = np.zeros((np.size(t),npsize(x)))
 
     return u, x, t   
 	

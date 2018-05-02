e458e152f28464980d83e7bfc460f19afc388351
Eric Riewski <riewski@wisc.edu>
2018-05-02 18:25:29 -0500

    Improvements to simulation results

--- main_Function_Simulation.py
+++ main_Function_Simulation.py
@@ -6,6 +6,9 @@ def main_Function_Simulation(user_input, u):
 
     u1 = u
     u2 = u
+
+    for i in range(user_input['Spatial Nodes']-1):
+        u[2,i] =u[1,i]+ 0.5 * c*c*(u[1,i+1] - 2*u[1,i]+ u[1,i-1]); #solution for the first time step
     
     for j in range(2,int(user_input['Temporal Nodes'])-1):
         for i in range(1,int(user_input['Spatial Nodes'])-1):
